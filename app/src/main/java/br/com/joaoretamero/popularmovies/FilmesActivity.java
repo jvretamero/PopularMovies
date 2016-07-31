314ddd93e8dbd65e4da9d9fd358850c50b04900b
João Vitor <joaovretamero@gmail.com>
2016-07-31 12:10:14 -0300

    Renomeado a MoviesActivity para FilmesActivity

--- app/src/main/java/br/com/joaoretamero/popularmovies/FilmesActivity.java
+++ app/src/main/java/br/com/joaoretamero/popularmovies/FilmesActivity.java
@@ -3,7 +3,7 @@ package br.com.joaoretamero.popularmovies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MoviesActivity extends AppCompatActivity {
+public class FilmesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
