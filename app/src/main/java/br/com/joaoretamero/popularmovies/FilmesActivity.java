f76bc004905366f70b75a2b83ad05ae3bbf4a490
João Vitor <joaovretamero@gmail.com>
2016-07-31 12:10:48 -0300

    Renomeado a activity_movies.xml para activity_filmes.xml

--- app/src/main/java/br/com/joaoretamero/popularmovies/FilmesActivity.java
+++ app/src/main/java/br/com/joaoretamero/popularmovies/FilmesActivity.java
@@ -8,6 +8,6 @@ public class FilmesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movies);
+        setContentView(R.layout.activity_filmes);
     }
 }
