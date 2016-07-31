2fdb7ce2d31e864b0e124eca1c41896cbfabf03e
João Vitor <joaovretamero@gmail.com>
2016-07-31 12:09:00 -0300

    Adicionado menu

--- app/src/main/java/br/com/joaoretamero/popularmovies/MoviesActivity.java
+++ app/src/main/java/br/com/joaoretamero/popularmovies/MoviesActivity.java
@@ -1,7 +1,7 @@
 package br.com.joaoretamero.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MoviesActivity extends AppCompatActivity {
 
