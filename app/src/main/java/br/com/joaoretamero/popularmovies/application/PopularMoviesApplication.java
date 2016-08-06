ba920ec6890fc83b2a2c219a5c27447f347c1245
João Vitor <joaovretamero@gmail.com>
2016-08-05 23:13:07 -0300

    Adicionado PopularMoviesApplication no manifest; Inserido configuração do Realm;

--- app/src/main/java/br/com/joaoretamero/popularmovies/application/PopularMoviesApplication.java
+++ app/src/main/java/br/com/joaoretamero/popularmovies/application/PopularMoviesApplication.java
@@ -2,5 +2,19 @@ package br.com.joaoretamero.popularmovies.application;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class PopularMoviesApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        RealmConfiguration.Builder configurationBuilder = new RealmConfiguration.Builder(this);
+        configurationBuilder.name("popmoviesdb");
+        configurationBuilder.schemaVersion(1l);
+
+        Realm.setDefaultConfiguration(configurationBuilder.build());
+    }
 }
