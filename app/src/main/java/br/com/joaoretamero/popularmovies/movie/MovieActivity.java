5eb9da6aa6e5df9f7b027308b2098ae005ec78eb
João Vitor <joaovretamero@gmail.com>
2016-08-08 22:40:25 -0300

    Adicionado o bind dos dados na MovieActivity

--- app/src/main/java/br/com/joaoretamero/popularmovies/movie/MovieActivity.java
+++ app/src/main/java/br/com/joaoretamero/popularmovies/movie/MovieActivity.java
@@ -1,17 +1,109 @@
 package br.com.joaoretamero.popularmovies.movie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatRatingBar;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import br.com.joaoretamero.popularmovies.R;
+import br.com.joaoretamero.popularmovies.model.Movie;
+import io.realm.Realm;
 
 public class MovieActivity extends AppCompatActivity {
 
     public static final String EXTRA_MOVIE_ID = "movie_id";
 
+    private ImageView backdrop;
+    private TextView title;
+    private AppCompatRatingBar ratingBar;
+    private TextView genres;
+    private TextView duration;
+    private TextView overview;
+    private TextView productionCompanies;
+    private RecyclerView videosList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        initToolbar();
+        initTitle();
+        initRatingBar();
+        initGenres();
+        initDuration();
+        initOverview();
+        initProductionCompanies();
+        initVideosList();
+
+        bindData();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initTitle() {
+        title = (TextView) findViewById(R.id.movie_title);
+    }
+
+    private void initRatingBar() {
+        ratingBar = (AppCompatRatingBar) findViewById(R.id.movie_vote_average);
+        ratingBar.setNumStars(10);
+        ratingBar.setStepSize(1f);
+        ratingBar.setRating(0f);
+    }
+
+    private void initGenres() {
+        genres = (TextView) findViewById(R.id.movie_genres);
+    }
+
+    private void initDuration() {
+        duration = (TextView) findViewById(R.id.movie_duration);
+    }
+
+    private void initOverview() {
+        overview = (TextView) findViewById(R.id.movie_overview);
+    }
+
+    private void initProductionCompanies() {
+        productionCompanies = (TextView) findViewById(R.id.movie_production_companies);
+    }
+
+    private void initVideosList() {
+        videosList = (RecyclerView) findViewById(R.id.movie_videos_list);
+        videosList.setLayoutManager(new LinearLayoutManager(MovieActivity.this, LinearLayoutManager.VERTICAL, false));
+        videosList.setItemAnimator(new DefaultItemAnimator());
+        videosList.setAdapter(null);
+    }
+
+    private void bindData() {
+        int movieId = 0;
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            movieId = intent.getIntExtra(EXTRA_MOVIE_ID, 0);
+        }
+
+        Realm realm = Realm.getDefaultInstance();
+        try {
+            Movie movie = realm.where(Movie.class).equalTo("id", movieId).findFirst();
+
+            title.setText(movie.title);
+            ratingBar.setRating(movie.voteAverage);
+            genres.setText("None");
+            duration.setText("0 min"); // TODO obter "runtime"
+            overview.setText(movie.overview);
+            productionCompanies.setText("None");
+        } finally {
+            realm.close();
+        }
     }
 }
