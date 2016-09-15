9df3eab92ba571794b94d42191da8060889838e9
João Vitor <joaovretamero@gmail.com>
2016-09-14 23:33:46 -0300

    Preparação para os testes:

---
+++ app/src/test/java/br/com/joaoretamero/popularmovies/TestUseCaseHandler.java
@@ -0,0 +1,12 @@
+package br.com.joaoretamero.popularmovies;
+
+import br.com.joaoretamero.popularmovies.domain.threading.UseCaseHandler;
+import br.com.joaoretamero.popularmovies.domain.usecase.UseCase;
+
+public class TestUseCaseHandler implements UseCaseHandler {
+    
+    @Override
+    public <T, H> void execute(UseCase<T, H> useCase, T requestValue, UseCase.Callback<H> callback) {
+        useCase.execute(requestValue);
+    }
+}
